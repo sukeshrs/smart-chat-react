9699645852a72ea70dc8f99ec0feab37565df4a7
sukeshrs <sukesrs@gmail.com>
2020-08-01 08:53:26 -0700

    smart chat react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="ui container comments" >
+      <div className="comment">
+        <a href="/" className = "avatar">
+          <img alt="avatar"/>
         </a>
-      </header>
+        <div className = "content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 PM</span>
+          </div>
+          <div className="text">Nice blog post</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
 export default App;
+
+
